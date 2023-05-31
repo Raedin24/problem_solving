2264c48686f79bf543a39cb278e9a1c376c80dfe
Raedin24 <lexhue24@gmail.com>
2023-05-31 10:22:43 +0000

    Day 7 solutions

---
+++ decrypt_string_mapping.py
@@ -0,0 +1,13 @@
+def freqAlphabets(s: str) -> str:
+    i = len(s) - 1
+    result = []
+    while i >= 0:
+        if s[i] == '#':
+            result.append(chr(int(s[i - 2:i]) + 96))
+            i -= 3
+        else:
+            result.append(chr(int(s[i]) + 96))
+            i -= 1
+    return ''.join(result[::-1])
+
+print(freqAlphabets("10#11#12"))
