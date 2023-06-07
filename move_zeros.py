31d66cacbec582d25914c2911a253f3126ea7281
Raedin24 <lexhue24@gmail.com>
2023-06-07 12:32:47 +0000

    Day 8 solutions

--- move_zeros.py
+++ move_zeros.py
@@ -6,16 +6,5 @@ def moveZeros(nums: list[int]) -> None:
     while c < i:
         nums.append(0)
         c += 1
-    
-        # if i == 0:
-        #     nums.pop(i)
-        #     # nums.append(i)
-        # else:
-        #     continue
 
     return nums
-
-print(moveZeros([0, 1, 0, 3, 12])) # 1, 3, 12, 0, 0
-print(moveZeros([0])) # 0
-print(moveZeros([0, 0, 1])) # 1, 0, 0
-print(moveZeros([0, 1, 0, 3, 12, 0, 0, 0, 0, 0, 0, 0])) # 1, 3, 12, 0, 0
