cc8e22c9adb33d5e877e2fdfaf86aa9fed480464
Raedin24 <lexhue24@gmail.com>
2023-06-14 17:22:21 +0000

    Contest 2

---
+++ CodeForces/restore_array.py
@@ -0,0 +1,14 @@
+t = int(input())
+test_cases = []
+for i in range(t):
+    n = int(input())
+    a = list(map(int, input().split()))
+    test_cases.append((n, a))
+
+for n, a in test_cases:
+    good_array = [0] * n
+    good_array[0] = a[0]
+    for i in range(n - 2):
+        good_array[i+1] = min(a[i], a[i + 1])
+    good_array[-1] = a[-1]
+    print(*good_array)
